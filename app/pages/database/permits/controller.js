ceaf973a88b97b58c65eef99d7aeb70dc66e415f
zerobstacle <bboyjong6989@gmail.com>
2019-04-04 10:53:24 +0800

    excel database

--- app/pages/database/permits/controller.js
+++ app/pages/database/permits/controller.js
@@ -5,6 +5,20 @@ myAppModule.controller('database_permit_controller', function ($scope, $timeout,
     $scope.xlxs_data = [];
     $scope.wsup_data = [];
     var uploading_type = '';
+    $scope.is_loading = false;
+    var PERMITS_DB = new JsonDB("DB/PERMITS", true, false);
+    try {
+        PERMITS_DB.getData("/wsup");
+    } catch(error) {
+        PERMITS_DB.push("/wsup",[]);
+    };
+
+    $scope.initialize_data = (t)=>{
+        let x = PERMITS_DB.getData("/"+t);
+        return ( x.length > 0 ) ? x : [];
+    }
+
+    $scope.check_empty = (t)=>{ return ( PERMITS_DB.getData("/"+t).length > 0 ) ? false : true }
 
     $scope.upload_excel = (f,t)=>{
         if(uploading_type != '') return null;
@@ -31,5 +45,57 @@ myAppModule.controller('database_permit_controller', function ($scope, $timeout,
         return (uploading_type == t) ? true : false;
     }
     
+    $scope.cancel_excel = (t)=>{
+        if(t=='wsup') $scope.wsup_data.splice(0,$scope.wsup_data.length);
+    }
+
+    var calculate_items = (d)=>{
+        let i = 0;
+        d.forEach(j => {
+            i = i + j.data.length;
+        });
+        return i;
+    }
+
+    $scope.save_database = (d,t)=>{
+        $scope.is_loading = true;
+        $scope.total_items = calculate_items(d);
+        $scope.pointer = 0;
+        var u = (sp,ip,dta)=>{
+            let q = { 
+                data : { 
+                    action : "database/permits/add",
+                    sheet : sp,
+                    item : ip,
+                    data_name : d[sp].name,
+                    data_item : JSON.stringify(d[sp].data[ip]),
+                    type : t,
+                    user_id : $scope.user.id
+                },
+                callBack : (data)=>{
+                    if(data.data.status == 1){
+                        $scope.pointer = $scope.pointer + 1;
+                        if($scope.total_items == $scope.pointer){
+                            $scope.is_loading = false;
+                            $scope.toast("Data saved online.");
+                            PERMITS_DB.push("/"+t,$scope.wsup_data);
+                            $scope.empty_data.wsup = false;
+                        }else {
+                            sp = ( d[sp].data.length == (ip + 1) ) ? (sp + 1) : sp;
+                            ip = ( d[sp].data.length == (ip + 1) ) ? 0 : (ip + 1);
+                            u(sp,ip);
+                        }
+                    }else {
+                        $scope.toast(data.data.error);
+                    }
+                },
+                errorCallBack : ()=>{
+                    $scope.toast("OFFLINE, try again later...");
+                }
+            };
+            $utils.api(q);
+        };
+        u(0,0);
+    }
 
 });
