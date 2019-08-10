962247e4802f7f64bd57ba5f610f7538fad3c933
akosiartas <akosiartas@gmail.com>
2019-08-11 01:24:33 +0800

    Added apprehension.html, updated controller.js

--- app/pages/database/controller.js
+++ app/pages/database/controller.js
@@ -1,4 +1,5 @@
 'use strict';
+
 document.write(`<script src="./plugins/chartjs/Chart.min.js"></script>`);
 var graphOption = {
     responsive: true,
@@ -31,7 +32,8 @@ var graphOption = {
         }]
     }
 };
-myAppModule.controller('pcsd_database_controller', function ($scope, $timeout, $utils, $mdToast,$localStorage) {
+myAppModule.controller('pcsd_database_controller', function ($scope, 
+    $timeout, $utils, $mdToast,$localStorage) {
     var XLSX = require('xlsx');
     $scope.wsup_db = { data : [], summary : {} };
     $scope.wsup_db.data = ($localStorage.wsup_db_data)?  $localStorage.wsup_db_data : [];
@@ -63,7 +65,10 @@ myAppModule.controller('pcsd_database_controller', function ($scope, $timeout, $
 
     $scope.getKeys = (data,key) => {
         let f = {};
-        data.map( d => { if(d[key]) f[d[key]] = true; return d[key]; } );
+        data.map( d => { 
+            if(d[key]) f[d[key]] = true; 
+            return d[key]; 
+        } );
         let r = [];
         let c = [];
         for (const key in f) {
@@ -179,58 +184,58 @@ myAppModule.controller('pcsd_database_controller', function ($scope, $timeout, $
     };
 
 
-    // $scope.upload_excel = (f,t)=>{
-    //     if(uploading_type != '') return null;
-    //     if(typeof(f) == typeof([])){
-    //         uploading_type = t;
-    //         var f = f[0];
-    //         var reader = new FileReader();
-    //         reader.onload = function(e) {
-    //             var data = e.target.result;
-    //             data = new Uint8Array(data);
-    //             let wb = XLSX.read(data, {type: 'array'});
-    //             wb.SheetNames.forEach(element => {
-    //                 let jsonData = XLSX.utils.sheet_to_json(wb.Sheets[element]);
-    //             });
-    //         };
-    //         reader.readAsArrayBuffer(f);
-    //     }else {
-    //         $scope.toast("file error");
-    //     }
-    // }
+    $scope.upload_excel = (files, permitType)=>{
+        if(uploading_type != '') return;
+        if(typeof(files) == typeof([])){
+            uploading_type = t;
+            var file = files[0];
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                var data = new Uint8Array(e.target.result);
+                let wb = XLSX.read(data, {type: 'array'});
+                wb.SheetNames.forEach(element => {
+                    let jsonData = XLSX.utils.sheet_to_json(wb.Sheets[element]);
+                });
+            };
+            reader.readAsArrayBuffer(file);
+        }else {
+            $scope.toast("file error");
+        }
 
-    $scope.save_database = (d,t)=>{
+    }
 
-        // async function createDatabase ()  {
-        //     await fire.db.database.query.doc("WSUP").set({"id":"WSUP"});
-        //     console.log("DB Created");
-        //     d[0].data.forEach( async (e) => {
-        //         let i = {};
-        //         for (const key in e) {
-        //             if (e.hasOwnProperty(key)) {
-        //                 const element = (e[key] != undefined)? e[key] : '';
-        //                 i[key] = element;
-        //             }
-        //         }
-        //         i.name = (e.First_Name  || '') + " " + (e.Middle_Name || '') + " " + (e.Last_Name || '') + " " + (e.Extension_Name || '');
-        //         i.address = (e.Street || '') + ", " + (e.Barangay || '') + ", " + (e.Municipality || '');
-        //         if(e.Issued_Year && e.Issued_Month && e.Issued_Day){
-        //             i.Issued_Date = e.Issued_Year + "-" + e.Issued_Month + "-" + e.Issued_Day;
-        //         }
-        //         if(e.Validity_Year && e.Validity_Month && e.Validity_Day) {
-        //             i.Validity_Date = e.Validity_Year + "-" + e.Validity_Month + "-" + e.Validity_Day;
-        //         }
-        //         i.keywords = i.name.split(' ').filter( d => d.length > 1);
-        //         await fire.db.database.query.doc("WSUP").collection("database").add(i);
-        //         let u = {};
-        //         u["count.all"] = firebase.firestore.FieldValue.increment(1);
-        //         if(e.Issued_Year != undefined) u[`count.${e.Issued_Year}.total`] = firebase.firestore.FieldValue.increment(1);
-        //         if(e.Issued_Month != undefined) u[`count.${e.Issued_Year}.${e.Issued_Month}`] = firebase.firestore.FieldValue.increment(1);
-        //         if(e.Municipality != undefined) u[`per_municipality.${e.Municipality}`] = firebase.firestore.FieldValue.increment(1);
-        //         await fire.db.database.query.doc("WSUP").update(u);
-        //     });
-        // };
-        // createDatabase();
+    $scope.save_database = (json, databaseName)=>{
+
+        async function createDatabase ()  {
+            await fire.db.database.query.doc("WSUP").set({"id":"WSUP"});
+            //console.log("DB Created");
+            json[0].data.forEach( async (e) => {
+                let i = {};
+                for (const key in e) {
+                    if (e.hasOwnProperty(key)) {
+                        const element = (e[key] != undefined)? e[key] : '';
+                        i[key] = element;
+                    }
+                }
+                i.name = (e.First_Name  || '') + " " + (e.Middle_Name || '') + " " + (e.Last_Name || '') + " " + (e.Extension_Name || '');
+                i.address = (e.Street || '') + ", " + (e.Barangay || '') + ", " + (e.Municipality || '');
+                if(e.Issued_Year && e.Issued_Month && e.Issued_Day){
+                    i.Issued_Date = e.Issued_Year + "-" + e.Issued_Month + "-" + e.Issued_Day;
+                }
+                if(e.Validity_Year && e.Validity_Month && e.Validity_Day) {
+                    i.Validity_Date = e.Validity_Year + "-" + e.Validity_Month + "-" + e.Validity_Day;
+                }
+                i.keywords = i.name.split(' ').filter( d => d.length > 1);
+                await fire.db.database.query.doc("WSUP").collection("database").add(i);
+                let u = {};//statisticss
+                u["count.all"] = firebase.firestore.FieldValue.increment(1);
+                if(e.Issued_Year != undefined) u[`count.${e.Issued_Year}.total`] = firebase.firestore.FieldValue.increment(1);
+                if(e.Issued_Month != undefined) u[`count.${e.Issued_Year}.${e.Issued_Month}`] = firebase.firestore.FieldValue.increment(1);
+                if(e.Municipality != undefined) u[`per_municipality.${e.Municipality}`] = firebase.firestore.FieldValue.increment(1);
+                await fire.db.database.query.doc("WSUP").update(u);
+            });
+        };
+        createDatabase();
         
 
         // let vv = [ 'Corporation ', 'Last_Name'];
@@ -246,6 +251,48 @@ myAppModule.controller('pcsd_database_controller', function ($scope, $timeout, $
 
         
     }
+}).
+controller('ApprehensionController', function($scope, $fireBaseService) {
+    $scope.apprehensionsTable = $scope.ngTable([]);
+    $fireBaseService.getApprehensions().then(apprehensions => {
+        $scope.apprehensionsTable = $scope.ngTable(apprehensions);
+    });
+    $scope.openApprehensionForm = (event) => {
 
-
+    }
+    $scope.Months = [
+        {id: 1, title: "January"},
+        {id: 2, title: "February"},
+        {id: 3, title: "March"},
+        {id: 4, title: "April"},
+        {id: 5, title: "May"},
+        {id: 6, title: "June"},
+        {id: 7, title: "July"},
+        {id: 8, title: "August"},
+        {id: 9, title: "September"},
+        {id: 10, title: "October"},
+        {id: 11, title:"November"},
+        {id: 12, title: "December"}
+    ];
+}).
+service('$fireBaseService', function() {
+    this.getApprehensions = () => {
+        var promise = new Promise((resolve, reject) => {
+            db.collection('database').
+                    doc('Apprehension').
+                    collection('apprehensions').
+                    onSnapshot(snapShot => {
+                        let apprehensions = snapShot.docs.map( documentSnapshot => {
+                            let apprehension = documentSnapshot.data();
+                            apprehension.id = documentSnapshot.id;
+                            return apprehension;
+                        });
+            
+                        resolve(apprehensions);
+                    }); 
+        });
+        return promise;
+    };
+    
 });
+;
