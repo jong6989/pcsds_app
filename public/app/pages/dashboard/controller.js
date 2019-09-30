a0922eee088ab1011a645491d186b18749176834
Vongamer <elitecabal001@gmail.com>
2019-09-30 10:08:01 +0800

    add application of import cert

--- public/app/pages/dashboard/controller.js
+++ public/app/pages/dashboard/controller.js
@@ -9,7 +9,8 @@ myAppModule.controller('applicant_dashboard_controller', function ($scope, $http
         { name : "Wildlife Special Use Permit (AO12)", url : "#!/pages/application/wildlife/wsup_ao12" },
         { name : "Wildlife Collector's Permit", url : "#!/pages/application/wildlife/wcp" },
         { name : "Wildlife Farm Permit", url : "#!/pages/application/wildlife/wfp" },
-        { name : "Wildlife Gratuitous Permit", url : "#!/pages/application/wildlife/gp" }
+        { name : "Wildlife Gratuitous Permit", url : "#!/pages/application/wildlife/gp" },
+        { name : "Wildlife Import Certification", url : "#!/pages/application/wildlife/wic" }
     ];
 
     $scope.load_my_applications = ()=>{
