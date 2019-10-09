8b4f5c7dbc70206a1271a3f44c6e9d827374eed6
bboyjong6989 <bboyjong6989@gmail.com>
2019-10-09 21:45:49 +0800

    account module

---
+++ public/staff/app/account_management/menus/controller.js
@@ -0,0 +1,22 @@
+'use strict';
+
+myAppModule.controller('menu_management_controller', function ($scope, $timeout, $mdDialog, $interval, $http, $localStorage) {
+    $scope.is_loading = false;
+    $scope.menu_modules = ($localStorage.brain_menu_modules)? $localStorage.brain_menu_modules : [];
+    
+    db.collection('module_menus').onSnapshot( qs => {
+        let results = qs.docs.map( d => {
+            let item = d.data();
+            item.id = d.id;
+            return item;
+        } );
+        $scope.menu_modules = results;
+        $localStorage.brain_menu_modules = results;
+        $scope.$apply();
+    });
+
+    $scope.add_menu_module = ()=>{
+        db.collection('module_menus').add({ title : '', icon : '', path: ''});
+    };
+
+});
--- public/staff/app/doc/settings.js
+++ public/staff/app/doc/settings.js
@@ -1,30 +1,10 @@
 // //  // Debug
-var doc_config = {
-	apiKey: "AIzaSyDJnCE34jNQ8mfQAcBt1zlGj5CJZwaOYfM",
-	authDomain: "pcsd-app.firebaseapp.com",
-	databaseURL: "https://pcsd-app.firebaseio.com",
-	projectId: "pcsd-app",
-	storageBucket: "pcsd-app.appspot.com",
-	messagingSenderId: "687215095072"
-};
 
-//// Realese
-// var doc_config = {
-// 	apiKey: "AIzaSyCELuc2f0_CcV35xeHid9-iFHU7hbrNPKg",
-// 	authDomain: "document-network.firebaseapp.com",
-// 	databaseURL: "https://document-network.firebaseio.com",
-// 	projectId: "document-network",
-// 	storageBucket: "document-network.appspot.com",
-// 	messagingSenderId: "583848541283"
-// };
-
-var docFire = firebase.initializeApp(doc_config, 'doc');
+var docFire = firebase;
 
 docFire.firestore().settings({
 	cacheSizeBytes: firebase.firestore.CACHE_SIZE_UNLIMITED
 });
-docFire.firestore().enablePersistence({synchronizeTabs:true});
-docFire.auth().signInWithEmailAndPassword('steve@pcsd.gov.ph', 'Pula6989~');
 var doc = {};
 doc.db = docFire.firestore();
 doc.fun = docFire.functions();
@@ -34,4 +14,3 @@ const agencies = 'agencies';
 const documents = 'documents';
 const doc_transactions = 'doc_transactions';
 const offlineFiles = 'offlineFiles';
-// const storageFolder = (os.platform() == 'win32')? app.getPath('downloads') + '\\document_network\\' : app.getPath('downloads') + '/document_network/';
