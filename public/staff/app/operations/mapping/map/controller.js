f4d1c4ed07cbcf5848d87d5075f57abb147afdc3
Jong Bautista <bboyjong6989@gmail.com>
2019-12-01 00:57:19 +0800

    mapbox

--- public/staff/app/operations/mapping/map/controller.js
+++ public/staff/app/operations/mapping/map/controller.js
@@ -3,17 +3,25 @@
 var map;
 var trigerMapFunctions;
 function initOpsMap() {
-    map = new google.maps.Map(document.getElementById('opsMap'), {
-        center: {lat: 9.81847614, lng: 118.74432172},
-        zoom: 10,
-        mapTypeId : 'satellite',
-        mapTypeControl : false
-    });
-    if(trigerMapFunctions) trigerMapFunctions();
+    mapboxgl.accessToken = "pk.eyJ1Ijoiam9uZzY5ODkiLCJhIjoiY2p5NjBkdnA5MDNneDNmcGt0eHVva2ZvZyJ9.jZwx_NUnKowJ4faIafJTew"
+    var map = new mapboxgl.Map({
+        // id: "hide"
+        container: 'opsMap',
+        style: 'mapbox://styles/jong6989/ck2u5e37k1phh1cs0nhhctx9c', 
+        center: [118.74432172, 9.81847614],
+        zoom: 50.7
+      });
+      
+    // map = new google.maps.Map(document.getElementById('opsMap'), {
+    //     center: {lat: 9.81847614, lng: 118.74432172},
+    //     zoom: 10,
+    //     mapTypeId : 'satellite',
+    //     mapTypeControl : false
+    // });
+    // if(trigerMapFunctions) trigerMapFunctions();
 }
-
 myAppModule.controller('opsMap_controller', function ($scope, $filter, $timeout, $mdDialog, $interval, $http, $localStorage) {
-    $scope.isLoading = true;
+    $scope.isLoading = false;
     
     var query;
     $scope.set_query = (q)=>{
@@ -47,6 +55,7 @@ myAppModule.controller('opsMap_controller', function ($scope, $filter, $timeout,
             }
         } );
     };
+    initOpsMap() ;
 
 });
 
