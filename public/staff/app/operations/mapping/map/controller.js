def58db3670ac2803d5f4c4dd72b12d295a7a209
Jong Bautista <bboyjong6989@gmail.com>
2019-12-01 02:38:00 +0800

    m

--- public/staff/app/operations/mapping/map/controller.js
+++ public/staff/app/operations/mapping/map/controller.js
@@ -1,61 +1,96 @@
 'use strict';
 
-var map;
-var trigerMapFunctions;
-function initOpsMap() {
-    mapboxgl.accessToken = "pk.eyJ1Ijoiam9uZzY5ODkiLCJhIjoiY2p5NjBkdnA5MDNneDNmcGt0eHVva2ZvZyJ9.jZwx_NUnKowJ4faIafJTew"
-    var map = new mapboxgl.Map({
-        // id: "hide"
-        container: 'opsMap',
-        style: 'mapbox://styles/jong6989/ck2u5e37k1phh1cs0nhhctx9c', 
-        center: [118.74432172, 9.81847614],
-        zoom: 50.7
-      });
-      
-    // map = new google.maps.Map(document.getElementById('opsMap'), {
-    //     center: {lat: 9.81847614, lng: 118.74432172},
-    //     zoom: 10,
-    //     mapTypeId : 'satellite',
-    //     mapTypeControl : false
-    // });
-    // if(trigerMapFunctions) trigerMapFunctions();
-}
 myAppModule.controller('opsMap_controller', function ($scope, $filter, $timeout, $mdDialog, $interval, $http, $localStorage) {
     $scope.isLoading = false;
-    
-    var query;
-    $scope.set_query = (q)=>{
-        query = q;
-    };
+    $scope.map = undefined
+    $scope.recordingList = [];
+    var recordingsRef = db.collection("ecan_app_recordings");
 
-    trigerMapFunctions = ()=>{
-        $scope.isLoading = false;
-        query.where("time",">=",1573109962615).onSnapshot( qs => {
+    recordingsRef.onSnapshot(qs => {
+        //listen to created recordings
+        if(!qs.empty){
+            let results = qs.docs.map( d => {
+                var item = d.data();
+                item.id = d.id
+                return item;
+            } );
+            $scope.recordingList = results;
+            $scope.$apply();
+        }
+    } );
+
+    $scope.showLine = (id)=>{
+        $scope.isLoading = true
+        recordingsRef.doc(id).collection("gps").onSnapshot(qs => {
             if(!qs.empty){
-                let linePoints = [];
-                $scope.points_count = qs.size;
-                qs.docs.map( d => {
-                    let document = d.data();
-                    linePoints.push({lat: document.latitude, lng: document.longitude});
-                    // let marker = new google.maps.Marker({
-                    //     position: {lat: document.latitude, lng: document.longitude} , 
-                    //     map: map, 
-                    //     title : "Speed : " + $filter('number')( document.speed * 3.6, 2) + " kph \n Accuracy : " + document.accuracy
-                    // });
+                let results = qs.docs.map( d => {
+                    var item = d.data();
+                    return [item.longitude,item.latitude];
                 } );
-                let linePath = new google.maps.Polyline({
-                    path: linePoints,
-                    geodesic: true,
-                    strokeColor: '#FF0000',
-                    strokeOpacity: 1.0,
-                    strokeWeight: 2
-                });
-                linePath.setMap(map);
-                $scope.$apply();
+                //add layer if not yet drawn on map
+                if($scope.map.getLayer(id) == undefined){
+                    $scope.addLineLayer(id,results,"#f00",8);
+                }
+                $scope.map.flyTo({center: results[0], zoom: 15});
             }
+            $scope.isLoading = false;
+            $scope.$apply();
         } );
     };
-    initOpsMap() ;
+    
+    $scope.initMapBoxMap = ()=>{
+        //timer for letting angularjs load first before the map
+        setTimeout(()=>{
+            mapboxgl.accessToken = "pk.eyJ1Ijoiam9uZzY5ODkiLCJhIjoiY2p5NjBkdnA5MDNneDNmcGt0eHVva2ZvZyJ9.jZwx_NUnKowJ4faIafJTew";
+            $scope.map = new mapboxgl.Map({
+                container: 'opsMap',
+                style: 'mapbox://styles/jong6989/ck2u5e37k1phh1cs0nhhctx9c', 
+                center: [118.74432172, 9.81847614],
+                zoom: 10
+            });
+            initLayers()
+        },200);
+    };
+
+    function initLayers(){
+        setTimeout(()=>{
+            $scope.hideLayer("public-land")
+        },500);
+    };
+
+    $scope.hideLayer = (id)=>{
+        $scope.map.setLayoutProperty(id, 'visibility', 'none');
+    };
+
+    $scope.showLayer = (id)=>{
+        $scope.map.setLayoutProperty(id, 'visibility', 'visible');
+    };
+
+    $scope.addLineLayer = (id,lineCoordinates,color,line)=>{
+        $scope.map.addLayer({
+            "id": id,
+            "type": "line",
+            "source": {
+            "type": "geojson",
+            "data": {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                    "type": "LineString",
+                    "coordinates": lineCoordinates
+                    }
+                }
+            },
+            "layout": {
+                "line-join": "round",
+                "line-cap": "round"
+            },
+            "paint": {
+                "line-color": color,
+                "line-width": line
+            }
+        });
+    };
 
 });
 
